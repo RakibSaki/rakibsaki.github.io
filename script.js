108084e21114b76990d1b4e1d23d3fe480f063de
Rakib Muhammed Saki <rakib1saki@gmail.com>
2022-01-13 22:13:33 +0600

    Fix nav link scroll and setup section background responsive to scroll

--- script.js
+++ script.js
@@ -23,8 +23,35 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     let ancs = document.querySelectorAll('a')
     for (let anc of ancs) {
-        if (anc.getAttribute("src") != "#") {
-            anc.style.color = "skyblue"
+        console.log(`${anc.innerHTML} is ${anc.getAttribute("href")}`)
+        if (anc.getAttribute("href")[0] != "#") {
+            anc.style.color = "blue"
+            anc.style.textDecoration = "underline"
         }
     }
+    let sectionHeight = innerHeight - 80
+    let navs = document.querySelectorAll('nav ul li a')
+    for (let i = 0; i < navs.length; i++) {
+        navs[i].addEventListener('click', e => {
+            e.preventDefault()
+            window.scrollTo(0, sectionHeight * i)
+        })
+    }
+    let sections = document.querySelectorAll('section')
+    function adjustSectionHues(scrollY) {
+        console.log(scrollY / sectionHeight)
+        for (let i = 0; i < sections.length; i++) {
+            if (scrollY < sectionHeight * (i - 1)) {
+                sections[i].style.setProperty('--scrollX', '0%')
+            } else if (scrollY > sectionHeight * (i + 1)) {
+                sections[i].style.setProperty('--scrollX', '100%')
+            } else {
+                sections[i].style.setProperty('--scrollX', `${50 + parseInt(50 * (scrollY - (sectionHeight * i)) / sectionHeight)}%`)
+            }
+            sections[i].style.background = sections[i].style.background
+        }
+    }
+    document.addEventListener('scroll', () => {
+        adjustSectionHues(window.scrollY)
+    })
 })
