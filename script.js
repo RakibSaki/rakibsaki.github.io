8fd4ef09aca48762b185695a8cfd03b4db4de777
Rakib Muhammed Saki <rakib1saki@gmail.com>
2022-01-13 13:20:35 +0600

    Add tea-cozy

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // let photo = document.querySelector('#home .image')
+    // function setPhoto() {
+    //     let [photoX] = [photo.clientWidth, photo.clientHeight]
+    //     let photoR = photoX / 2
+    //     let photoB = 3
+    //     photo.style.borderTopRightRadius = `${photoR}px`
+    //     photo.style.borderTopLeftRadius = `${photoR}px`
+    //     //photo.style.border = `${photoB}px solid transparent`
+    // }
+    // setPhoto()
+})
