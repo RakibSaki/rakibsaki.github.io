77df72334e80f3c314e4456a3519471b50193053
Rakib Muhammed Saki <rakib1saki@gmail.com>
2022-01-13 14:51:30 +0600

    Add alert for not done

--- script.js
+++ script.js
@@ -9,4 +9,22 @@ document.addEventListener('DOMContentLoaded', () => {
     //     //photo.style.border = `${photoB}px solid transparent`
     // }
     // setPhoto()
+    let alertE = document.querySelector('.alert')
+    alertE.style.animation = `1s appear`
+    setTimeout(() => {
+        alertE.style.opacity = '1'
+    })
+    let abutton = document.querySelector('#alert-button')
+    abutton.addEventListener('click', () => {
+        alertE.style.animation = `500ms disappear`
+        setTimeout(() => {
+            alertE.style.display = 'none'
+        }, 500)
+    })
+    let ancs = document.querySelectorAll('a')
+    for (let anc of ancs) {
+        if (anc.getAttribute("src") != "#") {
+            anc.style.color = "skyblue"
+        }
+    }
 })
