b8ecbf10cf3dfa7511ea18ed1e6e19562b8d5f88
Rakib Muhammed Saki <rakib1saki@gmail.com>
2022-01-28 18:15:06 +0600

    Fix scroll adjust and test colors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const backColor = document.querySelector('#back-color')
     let mainHeight = innerHeight - 80;
-    let totalScrollHeight = mainHeight * 5;
+    let totalScrollHeight = 3000;
     let colors = ['var(--yellow)', 'var(--magenta)', 'var(--blue)', 'var(--lime)']
     let backColorGradient = backColor.style.backgroundImage;
     function doSomething(scrollPos) {
@@ -14,9 +14,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     let [lastKnownScrollPosition, ticking] = [0, false]
     const main = document.querySelector('main')
-
-    let adjusted = false;
     main.addEventListener('scroll', function(e) {
+        if (lastKnownScrollPosition % mainHeight != 0 && main.scrollTop % mainHeight != 0) {
+            if (parseInt(lastKnownScrollPosition / mainHeight) != parseInt(main.scrollTop / mainHeight)) {
+                let scrollTo
+                if (main.scrollTop > lastKnownScrollPosition) {
+                    scrollTo = mainHeight * parseInt(main.scrollTop / mainHeight)
+                } else {
+                    scrollTo = mainHeight * parseInt(lastKnownScrollPosition / mainHeight)
+                }
+                main.scroll(0, scrollTo)
+            }
+        }
         lastKnownScrollPosition = main.scrollTop;
         if (!ticking) {
           window.requestAnimationFrame(function() {
@@ -25,24 +34,6 @@ document.addEventListener('DOMContentLoaded', () => {
           });
       
           ticking = true;
-        }
-        console.log(adjusted)
-        let adjustedSensitivity = 80
-        if (!adjusted) {
-            if (lastKnownScrollPosition % mainHeight < adjustedSensitivity) {
-                main.scroll(0, lastKnownScrollPosition - (lastKnownScrollPosition % mainHeight))
-                adjusted = true
-            } else if (lastKnownScrollPosition % mainHeight > mainHeight - adjustedSensitivity) {
-                console.log('am close')
-                main.scroll(0, lastKnownScrollPosition + mainHeight - (lastKnownScrollPosition % mainHeight))
-                adjusted = true
-            }
-        } else {
-            if (lastKnownScrollPosition % mainHeight < adjustedSensitivity) {
-            } else if (lastKnownScrollPosition % mainHeight > mainHeight - adjustedSensitivity) {
-            } else {
-                adjusted = false
-            }
         }
       });
 })
